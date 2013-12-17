a221427749e28cbfb1f88ded06b51fddd7ab9578
Elena <vadim@MacBook-Air.local>
2013-12-17 10:50:27 +0700

    Some changes.

--- src/motion/motion.c
+++ src/motion/motion.c
@@ -11,9 +11,9 @@
     \param h	The imahe height.
     \param br	The border on the image ing, new w = w + 2*br and new h = h + 2*br;
 */
-void find_object(uint32 *ing, uint16 *out, uint32 *hist, const int w, const int h, const int br, Object *obj)
+void find_object(uint32 *ing, uint16 *out, uint32 *hist, const int w, const int h, const int br, const int th, Object *obj)
 {
-    int i, x, y, yw, yw1, yx, yx1, w1 = w + br*2, cn;
+    int i, j=0, x, y, yw, yw1, yx, yx1, w1 = w + br*2, cn;
     int ox = obj->sz.x, oy = obj->sz.y, oxh = (obj->sz.x>>1)+1, oyh = (obj->sz.y>>1)+1;
     int dr[8] = {-1, -1+w, -w, 1-w, 1, 1+w, w, -1+w};
     //uint16 lm[w*h];     //Local max array
@@ -21,7 +21,7 @@ void find_object(uint32 *ing, uint16 *out, uint32 *hist, const int w, const int 
     Object oba[64];     //Objects array
     Object *obp[w*h];
 
-    memset(hist, 0, sizeof(hist));
+    memset(hist, 0, sizeof(uint32)*2048);
 
     for(y=0; y < h; y++){
         yw = y*w;
@@ -31,23 +31,24 @@ void find_object(uint32 *ing, uint16 *out, uint32 *hist, const int w, const int 
             yx1 = yw1 + (x + br - oxh);
             out[yx] = (ing[yx1 + ox + oy*w1] + ing[yx1] - ing[yx1 + ox] - ing[yx1 + oy*w1]);
             //Update histogram
-            hist[out[yx] ]++;
+            hist[out[yx]]++;
          }
     }
 
     //Find local max
     for(y=1; y < h-1; y++){
         yw = y*w;
-        for(x=1; x < w; x++){
+        for(x=1; x < w-1; x++){
             yx = yw + x;
             cn = 0;
             for(i=0; i < 8; i++) if(out[yx] >= out[yx + dr[i]]) cn++;
             //Local max
-            if(cn == 8) {
-
+            if(cn == 8 && out[yx] > th) {
+                oba[j].nc = out[yx];
+                oba[j].cg.x = x;
+                oba[j].cg.y = y;
+                j++;
             }
          }
     }
-
-
 }
